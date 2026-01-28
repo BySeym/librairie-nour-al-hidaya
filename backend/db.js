db2877ab216ad21ba6b9783c8453701c6ae0ad17
BySeym <oussa.bacha@outlook.fr>
2026-01-28 14:38:28 +0100

    Sécurisation: utilisation des variables d'environnement pour MySQL

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ import mysql from "mysql2/promise";
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "19the79Wall.*",
   database: "librairie_nah",
   waitForConnections: true,
   connectionLimit: 10,
